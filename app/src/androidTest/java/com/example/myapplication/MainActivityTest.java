5de4529909e29e921ac71e2d5d8d7e6fd32dcdb2
StopMakingUsernamesUnique <kfmc2001@gmail.com>
2020-05-05 13:10:48 -0700

    Finish Assingment 3, Added some Tests

---
+++ app/src/androidTest/java/com/example/myapplication/MainActivityTest.java
@@ -0,0 +1,65 @@
+package com.example.myapplication;
+
+import android.widget.DatePicker;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.equalTo;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+    @Test
+    public void hasTextOnScreen() {
+        onView(withId(R.id.Coffee)).check(matches(withText(R.string.coffee_dates)));
+        onView(withId(R.id.Image)).check(matches(isDisplayed()));
+        onView(withId(R.id.Name)).check(matches(isDisplayed()));
+        onView(withId(R.id.Bio)).check(matches(isDisplayed()));
+        onView(withId(R.id.Work)).check(matches(isDisplayed()));
+        onView(withId(R.id.Submit)).check(matches(isDisplayed()));
+
+
+    }
+
+    @Test
+    public void hasSecondActivity() {
+        onView(withId(R.id.Name)).perform(click())
+                .perform(typeText("Barry"));
+        onView(withId(R.id.Age)).perform(click())
+                .perform(typeText("42"));
+        onView(withId(R.id.Bio)).perform(click())
+                .perform(typeText("Barry Bio"));
+        onView(withId(R.id.Work)).perform(click())
+                .perform(typeText("Barry Work"));
+
+        onView(withId(R.id.Submit)).perform(click());
+
+        onView(withId(R.id.Bioplate)).check(matches(withText(R.string.about_me)));
+        onView(withId(R.id.Jobplate)).check(matches(withText(R.string.my_field)));
+
+        onView(withId(R.id.Nameplate)).check(matches(withText("Barry")));
+        onView(withId(R.id.Ageplate)).check(matches(withText("42")));
+        onView(withId(R.id.Bioblock)).check(matches(withText("Barry Bio")));
+        onView(withId(R.id.Nameplate)).check(matches(withText("Barry Work")));
+
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+
+import java.text.ParseException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +18,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void toCheckValues(View view) throws ParseException {
+
+        EditText Bio = (EditText) findViewById(R.id.Bio);
+        EditText Name = (EditText) findViewById(R.id.Name);
+        EditText Age = (EditText) findViewById(R.id.Age);
+        EditText Work = (EditText) findViewById(R.id.Work);
+
+        if((Bio.getText().toString().isEmpty())){
+            Bio.setError("Empty Field");
+        }
+        if((Name.getText().toString().isEmpty())){
+            Name.setError("Empty Field");
+        }
+        if((Age.getText().toString().isEmpty())){
+            Age.setError("Empty Field");
+        }
+        if((Work.getText().toString().isEmpty())){
+            Bio.setError("Empty Field");
+        }
+
+        if( !(Bio.getText().toString().isEmpty()) && !(Name.getText().toString().isEmpty()) && !(Work.getText().toString().isEmpty()) && !(Age.getText().toString().isEmpty())) {
+
+
+
+            Intent intent = new Intent(MainActivity.this, checkValues.class);
+            intent.putExtra(Constants.NAME, Name.getText().toString());
+            intent.putExtra(Constants.BIO, Bio.getText().toString());
+            intent.putExtra(Constants.AGE, Age.getText().toString());
+            intent.putExtra(Constants.WORK, Work.getText().toString());
+
+            Bio.setText("");
+            Name.setText("");
+            Age.setText("");
+            Work.setText("");
+
+
+            startActivity(intent);
+
+        }
+
+    }
+
+
 }
+
